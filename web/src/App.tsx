7f6d9fd4208e66bc050e7d643c6c5bb4bd39aecd
Rudra-hacker-25 <120879978+Rudra-hacker-25@users.noreply.github.com>
2026-02-16 20:41:48 +0530

    Fix: Add basename to BrowserRouter for /WhatsBotX/ subdirectory routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -53,7 +53,7 @@ const App = () => {
               <ScrollProgress />
               <ToasterLegacy />
               <Toaster />
-              <BrowserRouter>
+              <BrowserRouter basename="/WhatsBotX">
                 <ScrollToTop />
                 <AnimatedRoutes />
               </BrowserRouter>
